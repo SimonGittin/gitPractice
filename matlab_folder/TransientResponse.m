59aa975fb09a1b6a2f16d09982df44ad07ea6451
SimonGitting <chenghan2018@email.com>
2024-01-01 13:35:30 +0800

    ch4 done without simulink

--- matlab_folder/TransientResponse.m
+++ matlab_folder/TransientResponse.m
@@ -2,7 +2,7 @@
 
 clc; clear; close all;
 
-%% 
+%% Second order parameters
 
 p1 = [1 3+7*i];
 p2 = [1 3-7*i];
@@ -30,7 +30,8 @@ num = [24.542];
 den = [1 4 24.542];
 T = tf(num, den)
 step(T)
-%% step response display SS
+
+%% step response  (SS form)
 
 A = [0 1 0; 0 0 1; -9 -8 -7];
 B = [7;8;9];
@@ -42,3 +43,36 @@ t = 0:0.1:10;
 
 step(T, t)
 grid on
+
+%% step response with SS with x(0) initial state vector
+
+syms s
+
+A = [0 1 0; 0 0 1; -24 -26 -9];
+B = [0; 0; 1];
+C = [1 1 0];
+X0 = [1; 0; 2];
+U = 1/(s+1); %exp(-t)
+
+
+I = eye(3);
+
+% visit (4.96) solve X
+X = ((s*I - A)^-1)*(X0 + B*U);
+
+% visit (4.97) solve Y
+Y = C*X;
+Y = simplify(Y)
+
+% show graph
+step(tf([1 11 5], [1 9 26 24]))
+
+% solve y(t)
+y = ilaplace(Y);
+pretty(y)
+
+% find eigenvalues of A which are poles of Y(s)
+eig(A)
+
+
+%% if U(s) is 1/s, graph becomes ramp line which is to be discussed
